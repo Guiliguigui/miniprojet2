9a74031e421dc428482fc788d56006bebc591d5f
Guillaume <gui.mairesse@gmail.com>
2019-10-30 16:29:56 +0100

    essai renommage avec panda

--- CSVTRAD.py
+++ CSVTRAD.py
@@ -1,8 +1,31 @@
 import csv
+import pandas
+
+format_trad={'address': 'adresse_titulaire',
+            'carrosserie': 'carrosserie',
+            'categorie': 'categorie',
+            'couleur': 'couleur',
+            'cylindree': 'cylindree',
+            'date_immat': 'date_immatriculation',
+            'denomination': 'denomination commerciale',
+            'energy': 'puissance',
+            'firstname': 'prenom',
+            'immat': 'immatriculation',
+            'marque': 'marque',
+            'name': 'nom',
+            'places': 'places',
+            'poids': 'poids',
+            'puissance': 'puissance',
+            'type_variante_version': 'type',
+            'vin': 'vin',
+}
 
 with open('auto.csv', 'r') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter='|')
-    for row in reader:
-        print(row)
+    reader = pandas.read_csv(csvfile, delimiter='|')
+    
+reader = reader.rename(format_trad, inplace=True)
+    
+print(reader)
+
 
   
--- tests/test_CSVTRAD.py
+++ tests/test_CSVTRAD.py
@@ -1,29 +1,29 @@
 import unittest
-from  CSVTRAD import lecture
-from  CSVTRAD import nommage_colonnes
-from  CSVTRAD import reorganisation_valeurs
+#from  CSVTRAD import lecture
+#from  CSVTRAD import nommage_colonnes
+#from  CSVTRAD import reorganisation_valeurs
 
 class TestCSVTRADFunctions(unittest.TestCase):
 
     def setUp(self):
 
-        format_trad={'address' :
-                     'carrosserie' :
-                     'categorie' :
-                     'couleur' :
-                     'cylindree' :
-                     'date_immat' :
-                     'denomination' :
-                     'energy' :
-                     'firstname',
-                     'immat',
-                     'marque',
-                     'name',
-                     'places',
-                     'poids',
-                     'puissance',
-                     'type_variante_version',
-                     'vin'
+        format_trad={'address' : 'adresse_titulaire',
+                     'carrosserie' : 'carrosserie',
+                     'categorie' : 'categorie',
+                     'couleur' : 'couleur',
+                     'cylindree' : 'cylindree',
+                     'date_immat' : 'date_immatriculation',
+                     'denomination' : 'denomination commerciale',
+                     'energy' : 'puissance',
+                     'firstname' : 'prenom',
+                     'immat' : 'immatriculation',
+                     'marque' : 'marque',
+                     'name' : 'nom',
+                     'places' : 'places',
+                     'poids' : 'poids',
+                     'puissance' : 'puissance',
+                     'type_variante_version' : 'type',
+                     'vin' : 'vin',
                      }
 
         format_actuel = ['address',
